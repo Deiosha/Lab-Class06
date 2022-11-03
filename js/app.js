b3bf40b23db93e2526ac4fc04e0abd98efab80ee
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2022-11-03 16:21:06 -0700

    added form

--- js/app.js
+++ js/app.js
@@ -1,5 +1,39 @@
 'use strict';
 
+// Form!! STEP 1: GET THE ELEMENT WE WANT TO LISTEN TO
+
+let myForm = document.getElementById('my-form');
+
+// STEP 3: CALLBACK FUNCTION!! DEFINE OUR EVENT LISTENER
+
+function handleSubmit(event) {
+  // DEFINE WHAT THE FUNCTION DOES
+  event.preventDefault();
+
+  let name = event.target.locationCity.value;
+  console.log(name);
+
+  let selection = event.target.favoriteCookies.value;
+  console.log(selection);
+
+  let photo = event.target.photo.value;
+  console.log('photo');
+
+  // Gather temperment info from our form
+  let LocationMin = event.target.min.checked;
+  let locationMax = event.taget.max.checked;
+  let locationAvg = event.tagert.average.checked;
+  myForm.reset();
+
+}
+
+// STEP 2: ADD EVENT LISTENER 
+
+myForm.addEventListener('submit', handleSubmit);
+
+
+
+
 let shopHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 let allShops = [];
@@ -11,8 +45,8 @@ let tableElem = document.getElementById('table');
 function randomCustomer(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 
-  function makeHeader(){
-    let tableRow = document.createElement('tr'); 
+  function makeHeader() {
+    let tableRow = document.createElement('tr');
     // <tr></tr> skip 3 because no need for text
     tableElem.appendChild(tableRow);
     // <table><tr></tr></table>
@@ -23,11 +57,11 @@ function randomCustomer(min, max) {
     tableRow.appendChild(firstCell);
     // <table> <tr> <td></td> </tr> </table>
 
-    for (let i = 0; i < shopHours.length; i ++) {
-      let tableData = document.createElement('td')
+    for (let i = 0; i < shopHours.length; i++) {
+      let tableData = document.createElement('td');
       // nothing in here. not in our html
       // next we give it content
-      tableData.textContent = shopHours[i]; 
+      tableData.textContent = shopHours[i];
       // the first loop will be 6am and so on...
       tableRow.appendChild(tableData);
     }
@@ -37,7 +71,7 @@ function randomCustomer(min, max) {
 
 // CONSTRUCTOR FUNCTION
 
-function Shop (locationName,locationMin, locationMax, locationAvg){
+function Shop(locationName, locationMin, locationMax, locationAvg) {
   this.locationName = locationName;
   this.locationMin = locationMin;
   this.locationMax = locationMax;
@@ -50,7 +84,7 @@ function Shop (locationName,locationMin, locationMax, locationAvg){
 
 // DEFINE PROTOTYPE
 
-Shop.prototype.getCookieNum = function() {
+Shop.prototype.getCookieNum = function () {
   for (let i = 0; i < shopHours.length; i++) {
     let customerNum = randomCustomer(this.locationMin, this.locationMax);
     //  40 
@@ -61,9 +95,9 @@ Shop.prototype.getCookieNum = function() {
     this.cookiesPerHour.push(cookieNum);
 
   }
-} ;
-Shop.prototype.render = function(){
-  let tableRow = document.createElement('tr'); 
+};
+Shop.prototype.render = function () {
+  let tableRow = document.createElement('tr');
   tableElem.appendChild(tableRow);
 
   let city = document.createElement('th');
@@ -71,35 +105,34 @@ Shop.prototype.render = function(){
   city.textContent = this.locationName;
   tableRow.appendChild(city);
 
-  for (let i = 0; i <this.cookiesPerHour.length; i ++) {
+  for (let i = 0; i < this.cookiesPerHour.length; i++) {
     let tableData = document.createElement('td');
-    tableData.textContent = this.cookieNum[i];
+    tableData.textContent = this.cookiesPerHour[i];
     tableRow.appendChild(tableData);
 
-    let totalRow = document.createElement('td');
-    tableRow.textContent = this.customersPerHour;
-    tableRow.appendChild(totalRow);
-  };
+  }
+  let total = document.createElement('td');
+  total.textContent = this.customersPerHour;
+  tableRow.appendChild(total);
+};
+let seattle = new Shop('Seattle', 23, 65, 6.3);
+let tokyo = new Shop('Tokyo', 3, 24, 1.2);
+let dubai = new Shop('Dubai', 11, 28, 3.7);
+let paris = new Shop('Paris', 20, 38, 2.3);
+let lima = new Shop('Lima', 2, 16, 4.6);
 
-  let seattle = new Shop('Seattle', 23, 65, 6.3);
-  let tokyo = new Shop('Tokyo', 3, 24, 1.2);
-  let dubai = new Shop('Dubai', 11, 28, 3.7);
-  let paris = new Shop('Paris', 20, 38, 2.3);
-  let lima = new Shop('Lima', 2 ,16, 4.6);
+console.log(allShops);
 
-  console.log(allShops);
-
-
-  function make() {
-    let tableRow = document.createElement('tr');
-    tableElem.appendChild(tableRow);
-    let firstCell = document.createElement('td');
-    tableRow.appendChild(firstCell);
-    for (let i = 0; i < shopHours.length; i++) {
-      let tableData = document.createElement('td');
-      tableData.textContent = shopHours[i];
-      tableRow.appendChild(tableData);
-    }
+function makeHeader() {
+  let tableRow = document.createElement('tr');
+  tableElem.appendChild(tableRow);
+  let firstCell = document.createElement('td');
+  tableRow.appendChild(firstCell);
+  for (let i = 0; i < shopHours.length; i++) {
+    let tableData = document.createElement('td');
+    tableData.textContent = shopHours[i];
+    tableRow.appendChild(tableData);
+  }
 }
 
 
@@ -115,8 +148,8 @@ function makeFooter() {
   for (let i = 0; i < shopHours.length; i++) {
     let hourlyTotal = 0;
 
-    for (let j = 0; j < allStores.length; j++) {
-      hourlyTotal += allStores[j].cookieNums[i];
+    for (let j = 0; j < allShops.length; j++) {
+      hourlyTotal += allShops[j].cookiesPerHour[i];
     }
 
     let tdElement = document.createElement('th');
@@ -132,24 +165,23 @@ function makeFooter() {
 
   tableElem.appendChild(tableRow);
 }
-}
 // #pragma: EXECUTABLE CODE
 
-make();
+makeHeader();
 
-seattle.rdmCookies();
+seattle.getCookieNum();
 seattle.render();
 
-tokyo.rdmCookies();
+tokyo.getCookieNum();
 tokyo.render();
 
-dubai.rdmCookies();
+dubai.getCookieNum();
 dubai.render();
 
-paris.rdmCookies();
+paris.getCookieNum();
 paris.render();
 
-lima.rdmCookies();
+lima.getCookieNum();
 lima.render();
 
 makeFooter();
